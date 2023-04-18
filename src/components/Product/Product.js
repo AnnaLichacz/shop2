541cd825d5b3aa8f0b18c55fe76ee46aea825e3c
Anna Lichacz <anniahod@wp.pl>
2023-04-18 17:22:40 +0200

    add files

---
+++ src/components/Product/Product.js
@@ -0,0 +1,52 @@
+import styles from './Product.module.scss';
+import ProductForm from '../ProductForm/ProductForm';
+import ProductImage from '../ProductImage/ProductImage';
+import PropTypes from 'prop-types';
+import { useState, useMemo } from 'react';
+
+const Product = ({name, colors, sizes, title, basePrice }) => {
+
+  const [currentColor, setCurrentColor] = useState(colors[0]);
+  const [currentSize, setCurrentSize] = useState(sizes[0]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  }  
+
+  const getPrice = useMemo(() => {
+    const found = sizes.find(element => element.name === currentSize)
+    return basePrice + found.additionalPrice;
+  }, [currentSize, basePrice, sizes]
+  );
+
+  return (
+    <article className={styles.product}>
+    <ProductImage name={name} title={title} currentColor={currentColor} />
+    <div>
+      <header>
+        <h2 className={styles.name}>{title}</h2>
+        <span className={styles.price}>Price: {getPrice} $</span>
+      </header>
+        <ProductForm
+          handleSubmit={handleSubmit}
+          colors={colors}
+          currentColor={currentColor}
+          setCurrentColor={setCurrentColor}
+          sizes={sizes}
+          currentSize={currentSize}
+          setCurrentSize={setCurrentSize} />
+    </div>
+  </article>
+  )
+};
+
+Product.propTypes= {
+  basePrice: PropTypes.number.isRequired,
+  colors: PropTypes.array.isRequired,
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  sizes: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default Product;
